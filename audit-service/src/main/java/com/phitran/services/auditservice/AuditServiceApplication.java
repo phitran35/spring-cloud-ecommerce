5a25b353070a91bf7a9e19fdefa5cdba05736e4c
phitran <thanhphi89@gmail.com>
2021-01-07 18:14:50 +0700

    audit service & dockerize

--- audit-service/src/main/java/com/phitran/services/auditservice/AuditServiceApplication.java
+++ audit-service/src/main/java/com/phitran/services/auditservice/AuditServiceApplication.java
@@ -5,6 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -17,6 +23,16 @@ public class AuditServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuditServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerPersonApi10() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.phitran.services.auditservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Audit API").description("Documentation Audit API v1.0").build());
     }
 
 }
--- audit-service/src/test/java/com/phitran/services/auditservice/AuditServiceApplicationTests.java
+++ audit-service/src/test/java/com/phitran/services/auditservice/AuditServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.phitran.services.auditservice;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
